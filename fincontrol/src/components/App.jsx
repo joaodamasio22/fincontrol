b106b1252fbc4da0b7400f3c584f08ff5c7aa888
João Damasio <joaodamasio039@gmail.com>
2026-04-28 13:30:51 -0300

    dsada

--- fincontrol/src/components/App.jsx
+++ fincontrol/src/components/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
+      <h1 className="text-4xl font-bold text-blue-600 text-center">
         FinControl 🚀
       </h1>
     </div>
